018798e5b65f247587239393af86d4ad578e0283
Daffa <daffa.wibowo@delameta.com>
2023-12-01 18:22:01 +0700

    lern

---
+++ navigation/lib/pages/page_dua.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+// import 'package:navigation/pages/page_satu.dart';
+
+class PageDua extends StatelessWidget {
+  const PageDua({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Dua'),
+      ),
+      body: Center(
+        child: Text(
+          'INI PAGE 2',
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: Icon(Icons.keyboard_arrow_left),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
---
+++ navigation/lib/pages/page_satu.dart
@@ -0,0 +1,36 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:navigation/pages/page_dua.dart';
+
+class PageSatu extends StatelessWidget {
+  const PageSatu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Satu'),
+      ),
+      body: Center(
+        child: Text(
+          'INI PAGE 1',
+          style: TextStyle(fontSize: 25),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return PageDua();
+              },
+            ),
+          );
+        },
+        child: Icon(Icons.keyboard_arrow_right),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
